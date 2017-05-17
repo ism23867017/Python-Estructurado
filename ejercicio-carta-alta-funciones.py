89058f09bc1ede516a6d464732c20c3ac48b5999
ism23867017 <fran.arrebola44@gmail.com>
2017-05-17 16:09:25 +0200

    Add files via upload

---
+++ ejercicio-carta-alta-funciones.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+j1 = input ("Introduzca un numero J1 :")
+j2 = input ("Introduzca un numero J2 :")
+
+
+
+
+def funcionj1():
+	if j1 > j2:
+		print "Gana J1"
+	elif j1<j2:
+		print "Gana J2"
+	else:
+		print "Empate"
+
+
+funcionj1()
