41c517ef71cac073534cc7627bda7f38adff3f8c
ism23867017 <fran.arrebola44@gmail.com>
2017-05-26 08:26:40 +0200

    Add files via upload

--- script.py
+++ script.py
@@ -2,15 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import os
+import stat
+path_to_explore="Documentos"
 
 
-for root, dirs, files in os.walk("Documentos"):
+for root, dirs, files in os.walk(path_to_explore):
 	
 	for name in files:
 		
-		name_path = os.path.join(root, name)
-		print name_path
-		print len(name_path), "b"
+		name_path = os.chmod(path_to_explore, stat.S_IRWXO)
 		
-		
-		
+print "Has cambiado los permisos de:", path_to_explore
